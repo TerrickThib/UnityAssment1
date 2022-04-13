8a64f411e3b571f82847ea46c483041e37cec920
Terrick Thibodeaux <terrickthibodeaux@gmail.com>
2022-04-13 16:49:36 -0500

    exta save

--- Assets/Scripts/GamePlay/PlayerDeathBehaviour.cs
+++ Assets/Scripts/GamePlay/PlayerDeathBehaviour.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerDeathBehaviour : MonoBehaviour
 {
     public GameObject Player;
-
+    Rigidbody 
     // Update is called once per frame
     void Update()
     {
